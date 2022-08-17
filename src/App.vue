0e2f1fde37003165cf0fdf1b1218887ede3e107c
abizmo <abizmo@gmail.com>
2022-08-17 12:49:03 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import Expenses from "./components/Expenses.vue";
+  import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <Expenses />
+  <Footer />
+</template>
+
+<style scoped></style>
---
+++ src/components/Expenses.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1>My balance $921.48</h1>
+
+  <h2>Spending - Last 7 days</h2>
+
+  <div class="chart">mon tue wed thu fri sat sun</div>
+
+  <div class="summary">
+    <div class="total">Total this month $478.33</div>
+    <div class="increase">+2.4% from last month</div>
+  </div>
+</template>
+
+<style scoped></style>
