0a889df2d8fc14639a233a00240cd40e84d026fb
Abián Izquierdo Montesdeoca <abizmo@gmail.com>
2022-08-22 10:24:56 +0100

    Update footer component (#4) (#9)

--- src/components/Expenses.vue
+++ src/components/Expenses.vue
@@ -4,18 +4,15 @@ import Body from './Body.vue';
 </script>
 
 <template>
-  <div class="container">
+  <div class="container expenses grid">
     <Header balance="921.48" />
     <Body />
   </div>
 </template>
 
 <style scoped>
-.container {
-  width: min(100% - 2rem, 34rem);
-  margin-inline: auto;
-  display: grid;
-  gap: var(--sizes-400);
+.expenses {
+  --gap: var(--sizes-400);
   margin-bottom: 5rem;
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,40 @@
 <script setup></script>
 <template>
-  <div class="attribution">
-    Challenge by
-    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-      >Frontend Mentor</a
-    >. Coded by <a href="#">Your Name Here</a>.
-  </div>
+  <footer>
+    <div class="container flex justify-content-between">
+      <div>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+          >Frontend Mentor</a
+        >
+      </div>
+      <div class="align-right">
+        Coded by <a href="https://www.abizmo.dev">Abián Izquierdo</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-  .attribution {
-    font-size: 11px;
-    text-align: center;
-  }
-  .attribution a {
-    color: hsl(228, 45%, 44%);
-  }
+footer {
+  font-size: var(--fs-200);
+  background-color: var(--clr-neutral-800);
+  position: fixed;
+  inset: auto 0 0;
+  color: var(--clr-neutral-200);
+  padding-block: var(--sizes-600);
+}
+
+a {
+  display: block;
+  color: var(--clr-secondary-500);
+  text-decoration: none;
+  text-transform: uppercase;
+  margin-top: var(--sizes-200);
+}
+
+a:hover {
+  text-decoration: underline;
+  filter: brightness(1.2);
+}
 </style>
