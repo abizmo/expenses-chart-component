8d64b07a1c44fdc1e251c9c13358cb99ba1c261e
Abián Izquierdo Montesdeoca <abizmo@gmail.com>
2022-08-23 12:10:55 +0100

    Add Balance component (#10)

---
+++ src/components/Balance.vue
@@ -0,0 +1,35 @@
+<script></script>
+
+<template>
+  <div class="balance">
+    <h1 class="balance--title">
+      My balance
+      <span>$<slot>0.00</slot> </span>
+    </h1>
+    <img class="balance--logo" src="../assets/logo.svg" alt="expenses" />
+  </div>
+</template>
+
+<style scoped>
+  .balance {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: var(--sizes-500);
+    background-color: var(--clr-primary-500);
+    border-radius: var(--sizes-200);
+  }
+  .balance--title {
+    display: grid;
+    gap: var(--sizes-200);
+    font-size: var(--fs-400);
+    color: var(--clr-neutral-200);
+  }
+  .balance--title span {
+    font-size: var(--fs-500);
+    font-weight: 700;
+  }
+  .balance--logo {
+    height: 40px;
+  }
+</style>
--- src/components/Expenses.vue
+++ src/components/Expenses.vue
@@ -1,18 +1,18 @@
 <script setup>
-import Header from './Header.vue';
-import Body from './Body.vue';
+  import Balance from "./Balance.vue";
+  import Body from "./Body.vue";
 </script>
 
 <template>
-  <div class="container expenses grid">
-    <Header balance="921.48" />
+  <div class="container grid expenses-container">
+    <Balance>921.48</Balance>
     <Body />
   </div>
 </template>
 
 <style scoped>
-.expenses {
-  --gap: var(--sizes-400);
-  margin-bottom: 5rem;
-}
+  .expenses-container {
+    --gap: var(--sizes-400);
+    margin-bottom: 5rem;
+  }
 </style>
